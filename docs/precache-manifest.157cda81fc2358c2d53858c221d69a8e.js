beec721f1af2c1495d62614d554b4ee6e4a22a9a
difo23 <diffozanzan@gmail.com>
2020-08-10 18:11:02 -0400

    Production build in docs

---
+++ docs/precache-manifest.157cda81fc2358c2d53858c221d69a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f47e51ca072a601479216e780459d18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a99590dd4442435d3f85",
+    "url": "/static/css/main.3570049a.chunk.css"
+  },
+  {
+    "revision": "bbcfa22e4cfa09999489",
+    "url": "/static/js/2.32f7fcd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.32f7fcd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99590dd4442435d3f85",
+    "url": "/static/js/main.c7fc3734.chunk.js"
+  },
+  {
+    "revision": "c2a4463da067d7ee0a4c",
+    "url": "/static/js/runtime-main.f3c16400.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import LikesApp from './components/Apps/LikesApp';
 import './component.css';
 
 ReactDOM.render(
-	<div>
-		<LikesApp />
+	<div className="container">
+		<div className="row">
+			<h1> Project #1 100daysofcode: Comments likes with react hooks</h1>
+			<hr />
+			<LikesApp />
+		</div>
 	</div>,
 	document.getElementById('root')
 );
